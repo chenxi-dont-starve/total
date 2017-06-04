5d2013c9bcdf059764d6efe35c6c627cbc6c2696
maliut <14302010030@fudan.edu.cn>
2017-06-04 18:48:55 +0800

    initial commit

---
+++ Cluster_3/Master/modoverrides.lua
@@ -0,0 +1,44 @@
+return {
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-721491336"]={
+    configuration_options={
+      ["Death Lv Drain (Trust)"]="Off",
+      ["General Winter"]="On",
+      ["Phoenix's Resilience"]="On",
+      ["Survivor's Guilt"]="On",
+      Trustworthy="On" 
+    },
+    enabled=true 
+  },
+  ["workshop-723721217"]={
+    configuration_options={ ["Reluctant Fighter"]="On", ["Serious Side"]="On", ["Truly an Angel"]="On" },
+    enabled=true 
+  },
+  ["workshop-727325644"]={
+    configuration_options={ ["Lightning Speed"]="On", ["Rely on Me!"]="On", ["Weigh Anchor"]="On" },
+    enabled=true 
+  },
+  ["workshop-729309574"]={
+    configuration_options={ ["Draw Fire"]="On", ["Nameship Leadership"]="On", ["Properly Dressed"]="On" },
+    enabled=true 
+  },
+  ["workshop-737441707"]={
+    configuration_options={ ["Calm Resolve"]="On", ["Guiding Glow"]="On", ["Unyielding Spirit"]="On" },
+    enabled=true 
+  },
+  ["workshop-897634060"]={
+    configuration_options={ ["Lucky Survivor"]="On", ["Priority Redirection"]="On", ["Soothing Rain"]="On" },
+    enabled=true 
+  } 
+}
---
+++ Cluster_4/Master/modoverrides.lua
@@ -0,0 +1,4 @@
+return {
+  ["workshop-367546858"]={ configuration_options={ LANG="auto", SMALL_TEXTURES=true }, enabled=true },
+  ["workshop-572538624"]={ configuration_options={  }, enabled=true } 
+}
